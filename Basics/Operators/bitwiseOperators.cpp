ab2366cfcb28c2a2de52b9dcd8141eead2d8ad75
verdan kumar <vardantharwani@gmail.com>
2025-01-26 19:59:50 +0500

    adding code for bitwise operators

---
+++ Basics/Operators/bitwiseOperators.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int a = 10;
+    int b = 20;
+    int c = 30;
+    int d = 40;
+
+    cout << (a & b) << endl;
+    cout << (5 & 4) << endl;
+    cout << (a | b) << endl;
+    cout << (a ^ b) << endl;
+    cout << (~a) << endl;
+    cout << (a << 2) << endl;
+    cout << (a >> 2) << endl;
+}
