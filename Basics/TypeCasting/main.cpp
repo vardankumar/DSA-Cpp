4e6c9a2dc21c784ece63ec6059d566901531d6b1
verdan kumar <vardantharwani@gmail.com>
2025-01-30 02:26:37 +0500

    adding typecasting code

---
+++ Basics/TypeCasting/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // INT TO FLOAT
+    // int num1 = 10;
+    // float num2 = 10.5;
+
+    // float result = num1 + num2;
+    // cout << result << endl;
+
+    // CH TO INT
+    // char ch = 'A';
+    // int num3 = ch;
+    // cout << num3 << endl;
+
+    // EXPLICIT TYPE CASTING
+    // int num1 = 10;
+    // float num2 = 10.5;
+
+    // float result = num1 + (int)num2;
+    // cout << result << endl;
+
+    // DOUBLE TO INT
+    // double pi = 3.14159265358979323846;
+    // int num4 = (int)pi;
+    // cout << num4 << endl;
+
+    //  FLOAT TO CHAR
+
+    // float floatNum = 65.65;
+    // char charNum = (char)floatNum;
+    // cout << charNum << endl;
+
+    // INT TO FLOAT
+    // int a = 10;
+    // float b = 3.0;
+    // float c = a / b;
+    // cout << c << endl;
+
+    return 0;
+}
