8def4f536a4ffbfd702114e92a49c1bcbc0b7ed9
verdan kumar <vardantharwani@gmail.com>
2025-01-30 10:46:09 +0500

    adding code for rectangle pattern

---
+++ Patterns/rectangle.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    for (int row = 0; row < 4; row++)
+    {
+        for (int col = 0; col < 6; col++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
