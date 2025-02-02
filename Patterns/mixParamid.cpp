555a213c3bcc209678ec70cafda4a8e00f8647ed
verdan kumar <vardantharwani@gmail.com>
2025-02-02 21:55:59 +0500

    adding code for mixed pattern

---
+++ Patterns/mixParamid.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+void firstHalf(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i; j++)
+        {
+            cout << "*";
+        }
+        for (int j = 0; j < 2 * i + 1; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 0; j < n - i; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
+void secondHalf(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i + 1; j++)
+        {
+            cout << "*";
+        }
+        for (int j = 0; j < 2 * n - 2 * i - 1; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 0; j < i + 1; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the number of rows: ";
+    cin >> n;
+    firstHalf(n);
+    secondHalf(n);
+
+    return 0;
+}
