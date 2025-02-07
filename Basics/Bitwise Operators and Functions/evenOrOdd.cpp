98ed3fc1526723f89ffcf0ea2ffec0785d7d6bda
verdan kumar <vardantharwani@gmail.com>
2025-02-08 00:11:30 +0500

    adding bitwise operators code and even or code with bitwise operator

---
+++ Basics/Bitwise Operators and Functions/evenOrOdd.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cin >> n;
+
+    if (n & 1 == 0)
+        cout << "Odd" << endl;
+    else
+        cout << "Even" << endl;
+
+    return 0;
+}
---
+++ Basics/Bitwise Operators and Functions/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << ~(5) << endl;
+    cout << (10 << 1) << endl;
+    cout << (10 << 2) << endl;
+    cout << (10 << 3) << endl;
+    cout << (-10 << 1) << endl;
+    cout << (10 >> 1) << endl;
+    cout << (10 >> 2) << endl;
+    cout << (5 & 3) << endl;
+    cout << (5 | 3) << endl;
+    cout << (5 ^ 5 ^ 9) << endl;
+    return 0;
+}
