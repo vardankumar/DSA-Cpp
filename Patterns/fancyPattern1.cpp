3eee5896516d176721df90aefdd9b8f0ae704d38
verdan kumar <vardantharwani@gmail.com>
2025-02-08 23:53:19 +0500

    adding code for fancyPattern1

--- Patterns/fancyPattern1.cpp
+++ Patterns/fancyPattern1.cpp
@@ -1,8 +1,37 @@
 #include <iostream>
 using namespace std;
 
+// ********1********
+// *******2*2*******
+// ******3*3*3******
+// *****4*4*4*4*****
+// ****5*5*5*5*5****
+// ***6*6*6*6*6*6***
+// **7*7*7*7*7*7*7**
+// *8*8*8*8*8*8*8*8*
+
 void fancyPattern1(int n)
 {
+    for (int i = 0; i < n; i++)
+    {
+        int start_num_index = 8 - i;
+        int num = i + 1;
+        int count_num = num;
+        for (int j = 0; j < 17; j++)
+        {
+            if (j == start_num_index && count_num > 0)
+            {
+                cout << num;
+                start_num_index += 2;
+                count_num--;
+            }
+            else
+            {
+                cout << "*";
+            }
+        }
+        cout << endl;
+    }
 }
 
 int main()
@@ -11,6 +40,7 @@ int main()
     int n;
     cout << "Enter the number of rows: ";
     cin >> n;
+    fancyPattern1(n);
 
     return 0;
 }
