c10311e99a0fb043b1b1abf3b299714b172485b1
verdan kumar <vardantharwani@gmail.com>
2025-02-08 23:28:41 +0500

    adding code for numeric palindromic equilateral pyramid pattern

---
+++ Patterns/fancyPattern1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void fancyPattern1(int n)
+{
+}
+
+int main()
+{
+
+    int n;
+    cout << "Enter the number of rows: ";
+    cin >> n;
+
+    return 0;
+}
--- Patterns/numericPalindromeEquilateralPyramid.cpp
+++ Patterns/numericPalindromeEquilateralPyramid.cpp
@@ -29,7 +29,7 @@ int main()
             }
             else if (j > n)
             {
-                cout << c;
+                cout << c--;
             }
         }
         k++;
