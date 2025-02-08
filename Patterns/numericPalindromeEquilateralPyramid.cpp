d104d1bfd38d978912d7f0ce230f310518ba7c08
verdan kumar <vardantharwani@gmail.com>
2025-02-08 23:08:07 +0500

    adding code for numierc palindromic equilateral pyramid pattern

--- Patterns/numericPalindromeEquilateralPyramid.cpp
+++ Patterns/numericPalindromeEquilateralPyramid.cpp
@@ -3,6 +3,38 @@ using namespace std;
 
 int main()
 {
+    int n;
+    cout << "Enter the number of rows: ";
+    cin >> n;
+    int k = n;
+    for (int i = 0; i < n; i++)
+    {
+        int c = 1;
+        for (int j = 0; j < k; j++)
+        {
+            if (j < n - i - 1)
+            {
+                cout << " ";
+            }
+            else if (j <= n - 1)
+            {
+                cout << c;
+                c++;
+            }
+            else if (j == n)
+            {
+                c -= 2;
+                cout << c;
+                c--;
+            }
+            else if (j > n)
+            {
+                cout << c;
+            }
+        }
+        k++;
+        cout << endl;
+    }
 
     return 0;
 }
