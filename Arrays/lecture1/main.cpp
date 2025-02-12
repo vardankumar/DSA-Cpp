596a4fca292c4a0a0574cf03977cc9b86b860a8c
verdan kumar <vardantharwani@gmail.com>
2025-02-12 21:50:45 +0500

    adding basic code for array declaration, initialization, accessing and fill

---
+++ Arrays/lecture1/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    // int arr[10];
+
+    // // error because no size;
+    // // int brr[];
+
+    // int brr[2] = {10, 20};
+    // int crr[] = {1, 2, 3, 4, 5};
+    // int drr[4] = {0};
+
+    // cout << drr[0] << endl;
+    // cout << drr[91] << endl;
+
+    int arr[4];
+    fill(arr, arr + 4, 23);
+    cout << arr[0] << endl;
+    cout << arr[1] << endl;
+    cout << arr[2] << endl;
+    cout << arr[3] << endl;
+
+    return 0;
+}
